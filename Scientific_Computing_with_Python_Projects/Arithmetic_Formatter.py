9aadc3d1a24457bcfbddb8b103d5e70128690e18
sagarsolanki1 <87610622+sagarsolanki1@users.noreply.github.com>
2022-09-29 19:05:04 +0530

    Update Arithmetic_Formatter.py

--- Scientific_Computing_with_Python_Projects/Arithmetic_Formatter.py
+++ Scientific_Computing_with_Python_Projects/Arithmetic_Formatter.py
@@ -1,54 +1,32 @@
 def arithmetic_arranger(lst):
-    lnklst=[]
-    t1=[]
-    t2=[]
-    t3=[]
-    for i in range(0,len(lst)):
-        lnklst.append(lst[i].split())
-    for i in range(len(lnklst)):
-        for j in range(3):
-            if j==0:
-                t1.append(lnklst[i][j])
-            elif j==1:
-                t2.append(lnklst[i][j])
-            else:
-                t3.append(lnklst[i][j])
-    for i in range(len(t1)):
+    t1,t2,t3,maxxterm,calc=[],[],[],[],[]
+    if len(lst)>5:
+        raise ValueError("Error: Too many problems.")
+    else:
+        for i in range(len(lst)):
+            temp=lst[i].split()
+            t1.append(temp[0])
+            t2.append(temp[1])
+            t3.append(temp[2])
+    length=len(t1)
+    for i in range(length):
         if len(t1[i])>len(t3[i]):
-            maxx=len(t1[i])
+            maxxterm.append(len(t1[i]))
         else:
-            maxx=len(t3[i])
-        if i==0:
-            print(" ",t1[i].rjust(maxx),"      ",end="")
-            continue
-        print(t1[i].rjust(maxx),"      ",end="")
-    print("\n")
-    for i in range(len(t1)):
-        if len(t1[i])>len(t3[i]):
-            maxx=len(t1[i])
-        else:
-            maxx=len(t3[i])
-        print(t2[i],t3[i].rjust(maxx),"    ",end="")
-    print("\n")
-    for i in range(len(t1)):
-        if len(t1[i])>len(t3[i]):
-            maxx=len(t1[i])
-        else:
-            maxx=len(t3[i])
-        print('-'*(maxx+2),"    ",end="")
-    print("\n")
-    for i in range(len(t1)):
-        if len(t1[i])>len(t3[i]):
-            maxx=len(t1[i])
-        else:
-            maxx=len(t3[i])
-        if i==0:
-            if t2[i]=='+':
-                print(" ",(int(t1[i])+int(t3[i])),"      ",end="")
-            elif t2[i]=='-':
-                print(" ",(int(t1[i])-int(t3[i])),"      ",end="")
-            continue
+            maxxterm.append(len(t3[i]))
+    for i in range(length):
+        print("  "+t1[i].rjust(maxxterm[i]),end="    ")
+    print("")
+    for i in range(length):
+        print(t2[i]+" "+t3[i].rjust(maxxterm[i]),end="    ")
+    print("")
+    for i in range(length):
+        print('-'*(maxxterm[i]+2),end="    ")
+    for i in range(length):
         if t2[i]=='+':
-            print((int(t1[i])+int(t3[i])),"      ",end="")
+            calc.append(int(t1[i])+int(t3[i]))
         elif t2[i]=='-':
-            print((int(t1[i])-int(t3[i])),"      ",end="")
+            calc.append(int(t1[i])-int(t3[i]))
+    print("")
+    for i in range(length):
+        print(str(calc[i]).rjust(maxxterm[i]+2),end="    ")
